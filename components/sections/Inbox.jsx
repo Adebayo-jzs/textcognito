15d01458bdcd2e32923ef9b0c766968b8d36f1d1
Adebayo-jzs <adedejiadebayo732@gmail.com>
2026-01-07 20:42:20 +0100

    landing page and ui improvement

--- components/sections/Inbox.jsx
+++ components/sections/Inbox.jsx
@@ -1,6 +1,6 @@
 export default function Inbox(){
     return(
-        <section className="bg-gradient-to-r from-pink-500 to-purple-600 dark:from-pink-900 dark:to-purple-900 rounded-t-[4rem] rounded-b-[4rem] relative z-10 py-32 -mt-16">
+        <section className="bg-gradient-to-r from-pink-500 to-purple-600   rounded-t-[4rem] rounded-b-[4rem] relative z-10 py-32 -mt-16">
             <div className="container mx-auto px-4 flex flex-col items-center justify-center text-center">
             <h2 className="text-6xl md:text-9xl font-black text-white tracking-tighter leading-[0.9]">
                 flood your<br />
