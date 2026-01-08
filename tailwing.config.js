091e72cd857143adbd25ba1d80ff239cc2dbc6eb
Adebayo-jzs <adedejiadebayo732@gmail.com>
2026-01-08 17:05:40 +0100

    landing page and ui improvement

---
+++ tailwing.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  darkMode: 'class', // This is crucial for manual toggling
+  theme: {
+    extend: {
+      fontFamily: {
+        // This sets Outfit as the default font for everything
+        sans: ['var(--font-outfit)', 'sans-serif'], 
+      },
+    },
+  },
+}
